33ea81b082e66d6b1feb35bd71fe63d0de0e9e9b
Ally Dian <141696851+allydian@users.noreply.github.com>
2025-06-12 12:03:29 +0800

    Added scene transitions and removed unused plugins
    - Added linear wipe for scene transitions in ChangeScenes.cs
    - Added TransitionManager prefab to every scene
    - Removed unused plugin files
    - Changed loading spinner

--- Assets/SceneController.cs
+++ Assets/SceneController.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using EasyTransition;
 
 public class SceneController : MonoBehaviour
 {
--- Assets/Scripts/UI/Scene Behaviours/ChangeScenes.cs
+++ Assets/Scripts/UI/Scene Behaviours/ChangeScenes.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using EasyTransition;
 
 /// <summary>
 /// A class responsible for changing scenes within the application.
@@ -9,36 +10,44 @@ using UnityEngine.SceneManagement;
 /// </summary>
 public class ChangeScenes : MonoBehaviour
 {
+    public TransitionSettings transitionSettings; // Assign this in inspector
+    public float transitionDelay = 0f;
+
     // Loads the main scene with panels (MainPanels scene) based on the previously opened scene.
     public void ToMainScene(AppPanelManager.PanelType panelType)
     {
         // Set the desired panel in the PanelManager
         AppPanelManager.instance.currentPanel = panelType;
-        
+
         // Now load the main scene
-        SceneManager.LoadScene("MainPanels (Test) 1");
+        //SceneManager.LoadScene("MainPanels (Test) 1");
+        TransitionManager.Instance().Transition("MainPanels (Test) 1", transitionSettings, transitionDelay);
     }
     public void ToMainPanels()
     {
-        SceneManager.LoadScene("MainPanels (Test) 1");
+        //SceneManager.LoadScene("MainPanels (Test) 1");
+        TransitionManager.Instance().Transition("MainPanels (Test) 1", transitionSettings, transitionDelay);
     }
 
     // Loads the AR camera scene.
     public void ToARCamera()
     {
-        SceneManager.LoadScene("ARcamera");
+        //SceneManager.LoadScene("ARcamera");
+        TransitionManager.Instance().Transition("ARcamera", transitionSettings, transitionDelay);
     }
 
     // Loads the scene for the Medicinal Plants Quiz.
     public void OpenMedicinalQuiz()
     {
-        SceneManager.LoadScene("MedicinalPlantsQuiz");
+        //SceneManager.LoadScene("MedicinalPlantsQuiz");
+        TransitionManager.Instance().Transition("MedicinalPlantsQuiz", transitionSettings, transitionDelay);
     }
 
     // Loads the AR quiz scene.
     public void ToARQuiz()
     {
-        SceneManager.LoadScene("ARquiz");
+        //SceneManager.LoadScene("ARquiz");
+        TransitionManager.Instance().Transition("ARquiz", transitionSettings, transitionDelay);
     }
 
     /// <summary>
@@ -48,7 +57,8 @@ public class ChangeScenes : MonoBehaviour
 
     public void ToBakoForest1()
     {
-        SceneManager.LoadScene("BakoForest 1");
+        //SceneManager.LoadScene("BakoForest 1");
+        TransitionManager.Instance().Transition("BakoForest 1", transitionSettings, transitionDelay);
     }
 
 }
